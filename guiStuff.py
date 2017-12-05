e4556185c8458da1c9a8358b30e4f69b676d955b
Kamin <kaminjfay@gmail.com>
2017-12-05 00:39:44 -0500

    Finalized Connection based on input from user
    Setup the input page, stock information to make my life easier. And began to setup for new bt page

--- guiStuff.py
+++ guiStuff.py
@@ -1,6 +1,10 @@
-import Tkinter as tk
+import tkinter as tk
+import globalVars
+from tableModification import dbMod
 
 LARGE_FONT = ("Verdana", 12)
+
+global db
 
 class tkTest(tk.Tk):
 
@@ -16,7 +20,7 @@ class tkTest(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, dbConfig):
+        for F in (StartPage, dbConfig, btLogger):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, stick="nsew")
@@ -26,6 +30,9 @@ class tkTest(tk.Tk):
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
+
+    def exit(self):
+        app.quit()
 
 
 class StartPage(tk.Frame):
@@ -38,6 +45,11 @@ class StartPage(tk.Frame):
 
         button1 = tk.Button(self, text="dbConfig", command=lambda: controller.show_frame(dbConfig))
         button1.pack()
+        button2 = tk.Button(self, text="Bluetooth", command=lambda: controller.show_frame(btLogger))
+        button2.pack()
+
+        exitButton = tk.Button(self, text="Exit", command=lambda: tkTest.quit(self))
+        exitButton.pack()
 
 class dbConfig(tk.Frame):
 
@@ -47,25 +59,51 @@ class dbConfig(tk.Frame):
         tk.Label(self, text="Password: ", font = LARGE_FONT).grid(row=1)
         tk.Label(self, text="Host: ", font = LARGE_FONT).grid(row=2)
         tk.Label(self, text="Database Name: ", font = LARGE_FONT).grid(row=3)
+        tk.Label(self, text="Port: ", font = LARGE_FONT).grid(row=4)
         e0 = tk.Entry(self)
+        e0.insert(0, 'alwayskamin')
         e0.grid(row=0, column=1)
         e1 = tk.Entry(self, show="*")
         e1.grid(row=1, column=1)
         e2 = tk.Entry(self)
+        e2.insert(0, 'db4free.net')
         e2.grid(row=2, column=1)
         e3 = tk.Entry(self)
+        e3.insert(0, 'btlogger')
         e3.grid(row=3, column=1)
+        e4 = tk.Entry(self)
+        e4.insert(0, '3307')
+        e4.grid(row=4, column=1)
 
-        tk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage)).grid(row=4, column=1, sticky="w")
-        tk.Button(self, text="Show", command=lambda: self.getInput(e0, e1, e2, e3)).grid(row=4, column=2, sticky="w")
+        tk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage)).grid(row=5, column=1, sticky="w")
+        tk.Button(self, text="Connect", command=lambda: self.getInput(e0, e1, e2, e3, e4)).grid(row=5, column=2, sticky="w")
 
-    def getInput(self, e0, e1, e2, e3):
+    def getInput(self, e0, e1, e2, e3, e4):
         print("Show data")
-        Username = e0.get()
-        Password = e1.get()
-        HostName = e2.get()
-        Database = e3.get()
-        print("User: " + Username + " pass: " + Password + " host: " + HostName + " databaseName: " + Database)
+        globalVars.username = e0.get()
+        globalVars.password = e1.get()
+        globalVars.hostname = e2.get()
+        globalVars.db = e3.get()
+        globalVars.port = int(e4.get())
+        db = dbMod()
+        print(dbMod.showVersion(db))
+
+class btLogger(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        tk.Button(self, text="Start", command=lambda: self.startScanning()).grid(row=0, column=1, sticky="w")
+        tk.Button(self, text="Stop", command=lambda: self.stopScanning()).grid(row=0, column=2, sticky="w")
+        tk.Button(self, text="Back", command=lambda: controller.show_frame(StartPage)).grid(row=0, column=3, sticky="w")
+
+
+
+    def startScanning(self):
+        print("Scanning")
+
+    def stopScanning(self):
+        print("Done Scanning")
 
 
 app = tkTest()
